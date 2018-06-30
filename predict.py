84f2d4a8cfb986fdccdd06094b7b20d8047b318d
louis8ok1 <louis8ok1@gmail.com>
2018-06-30 13:51:37 +0800

    Final

--- predict.py
+++ predict.py
@@ -25,42 +25,56 @@ from sklearn.utils import shuffle
 X = []
 Y = []
 
-#fullpath = str(sys.argv[1])
-print("\nPlease input data name:")
-path=input()
-fullpath=("./Face Database/"+path)
-print(fullpath)
-
-X = mpimg.imread(fullpath) / 255 * 2 - 1
-X = X[np.newaxis, ...]
-Y = fullpath[-9:-7]
-
-
-print("Training Data Shape: ", X.shape)
-
 model = load_model('train_model.h5')
 model.summary()
-result = model.predict(X)
-print("Predict result: ")
-print(result[0])
-print("\nTrue Answer: ")
-print(Y)
 
-result_dict = {}
-for i in range(len(result[0])):
-    result_dict[i] = float(result[0][i])
-sorted_result_dict = sorted(result_dict.items(), key=operator.itemgetter(1), reverse=True)
-cnt = 0
 
-print("\nPredict Answer:")
-if(sorted_result_dict[0][0]<10):
-    print("0"+str(sorted_result_dict[0][0]))
-else:
-    print(sorted_result_dict[0][0])
+for root, dirs, files in walk("test_data/"):
+    for f in files:
+        fullpath = join(root, f)
 
-print('\nTop 5 candidate: ')
-for tp in sorted_result_dict:
-    cnt += 1
-    print(str(tp[0])+' Possibility: %0.6f' %(tp[1]))
-    if cnt >= 5:
-        break
+        Y = fullpath[-9:-7]
+
+        X = mpimg.imread(fullpath) / 255 * 2 - 1
+
+
+        #fullpath = str(sys.argv[1])
+        # print("\nPlease input data name:")
+        # path=input()
+        # fullpath=("./Face Database/"+path)
+        # print(fullpath)
+
+        Test_Label  = np.array(Y)
+
+        Test_Data = X[np.newaxis, ...]
+
+
+        print("Training Data Shape: ", X.shape)
+
+        
+        result = model.predict(Test_Data)
+
+        print("Predict result: ")
+        print(result[0])
+        print("\nTrue Answer: ")
+        print(Y)
+
+        result_dict = {}
+        for i in range(len(result[0])):
+            result_dict[i] = float(result[0][i])
+        sorted_result_dict = sorted(result_dict.items(), key=operator.itemgetter(1), reverse=True)
+        cnt = 0
+
+        print("\nPredict Answer:")
+        if(sorted_result_dict[0][0]<10):
+            print("0"+str(sorted_result_dict[0][0]))
+        else:
+            print(sorted_result_dict[0][0])
+
+        print('\nTop 5 candidate: ')
+        for tp in sorted_result_dict:
+            cnt += 1
+            print(str(tp[0])+' Possibility: %0.6f' %(tp[1]))
+            if cnt >= 5:
+                break
+        wait = input("Press Any Key")
